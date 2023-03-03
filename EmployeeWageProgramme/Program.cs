6501eb34c3bb9c58fafbc826d39d3842f7dc44d7
Poojachakor <chakorpooja07@gmail.com>
2023-03-03 19:33:23 +0530

    Add: UC9 file

--- EmployeeWageProgramme/Program.cs
+++ EmployeeWageProgramme/Program.cs
@@ -4,17 +4,31 @@ namespace EmployeeWageProgramme
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
 
+        const int isFullTime = 1;
+        const int isPartTime = 2;
+
+        public static void Main(string[] args)
+        {
             Console.WriteLine("....EMP WAGE COMPUTATION....");
 
             //UC7-Use Class Method and Variables
+            //UC8-Multiple companies
+            EmpWageBuilder Tata = new EmpWageBuilder("HCL", 30, 16, 60);
+            EmpWageBuilder Google = new EmpWageBuilder("TCS", 20, 15, 80);
+            EmpWageBuilder Apple = new EmpWageBuilder("BRIDGELAB", 15, 18, 100);
 
-            EmpWageForMultipleCompanies.EmpWage("HCL", 30, 16, 60);
-            EmpWageForMultipleCompanies.EmpWage("TCS", 20, 15, 80);
-            EmpWageForMultipleCompanies.EmpWage("BRIDGELAB", 15, 18, 100);
+            Tata.EmpWage();
+            Console.WriteLine(Tata.ToString());
+            Google.EmpWage();
+            Console.WriteLine(Apple.ToString());
+            Apple.EmpWage();
+            Console.WriteLine(Google.ToString());
+
             Console.ReadLine();
+
+
+
         }
     }
 }
