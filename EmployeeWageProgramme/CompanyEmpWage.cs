84e461ba9a8aef347c5fb9fe9619368b212f71b0
Poojachakor <chakorpooja07@gmail.com>
2023-03-03 20:17:27 +0530

    Add: UC11 file

--- EmployeeWageProgramme/CompanyEmpWage.cs
+++ EmployeeWageProgramme/CompanyEmpWage.cs
@@ -6,30 +6,32 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageProgramme
 {
+    public interface IEmpWage
+    {
+        void AddCompanyEmpWage(string company, int empWagePerHr, int empWorkinDaysPerMonth, int empTotalWorkingHrs);
+        void IterateOverCompanies();
+    }
     internal class CompanyEmpWage
     {
-        
-            public string company;
-            public int empWagePerHr;
-            public int empWorkinDaysPerMonth;
-            public int empTotalWorkingHrs;
-            public int empMontlyWage;
-            public CompanyEmpWage(string company, int empWagePerHr, int empWorkinDaysPerMonth, int empTotalWorkingHrs)
-            {
-                this.company = company;
-                this.empWagePerHr = empWagePerHr;
-                this.empWorkinDaysPerMonth = empWorkinDaysPerMonth;
-                this.empTotalWorkingHrs = empTotalWorkingHrs;
-            }
-            public void SetTotalEmpWage(int empMontlyWage)
-            {
-                this.empMontlyWage = empMontlyWage;
-            }
-            public override string ToString()
-            {
-                return "Company :" + this.company + " TotalWage =" + this.empMontlyWage;
-            }
-        
+        public string company;
+        public int empWagePerHr;
+        public int empWorkinDaysPerMonth;
+        public int empTotalWorkingHrs;
+        public int empMontlyWage;
+        public CompanyEmpWage(string company, int empWagePerHr, int empWorkinDaysPerMonth, int empTotalWorkingHrs)
+        {
+            this.company = company;
+            this.empWagePerHr = empWagePerHr;
+            this.empWorkinDaysPerMonth = empWorkinDaysPerMonth;
+            this.empTotalWorkingHrs = empTotalWorkingHrs;
+        }
+        public void SetTotalEmpWage(int empMontlyWage)
+        {
+            this.empMontlyWage = empMontlyWage;
+        }
+        public override string ToString()
+        {
+            return "Company :" + this.company + " TotalWage =" + this.empMontlyWage;
+        }
     }
 }
-
--- EmployeeWageProgramme/Program.cs
+++ EmployeeWageProgramme/Program.cs
@@ -5,25 +5,17 @@ namespace EmployeeWageProgramme
     internal class Program
     {
 
-        const int isFullTime = 1;
-        const int isPartTime = 2;
-
         public static void Main(string[] args)
         {
-            Console.WriteLine("  Welcome to Employee Wage Computation Problem");
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
             //UC7-Use Class Method and Variables
             //UC8-Multiple companies
-
-
-
-            EmpWageBuilder builder = new EmpWageBuilder();
-
+            IEmpWage builder = new EmpWageBuilder();
 
             builder.AddCompanyEmpWage("HCL", 30, 16, 60);
             builder.AddCompanyEmpWage("TCS", 20, 15, 80);
             builder.AddCompanyEmpWage("BRIDGELAB", 15, 18, 100);
             builder.IterateOverCompanies();
-            Console.WriteLine("Done");
             Console.ReadLine();
 
         }
