40f59017d1b67b31d9ea9153c4fa07cbea0a6c95
Poojachakor <chakorpooja07@gmail.com>
2023-03-03 19:13:19 +0530

    Add: UC8 file

--- EmployeeWageProgramme/Program.cs
+++ EmployeeWageProgramme/Program.cs
@@ -6,7 +6,14 @@ namespace EmployeeWageProgramme
     {
         static void Main(string[] args)
         {
-            ComputeEmpWage.ComputeEmployeeWage();
+
+            Console.WriteLine("....EMP WAGE COMPUTATION....");
+
+            //UC7-Use Class Method and Variables
+
+            EmpWageForMultipleCompanies.EmpWage("HCL", 30, 16, 60);
+            EmpWageForMultipleCompanies.EmpWage("TCS", 20, 15, 80);
+            EmpWageForMultipleCompanies.EmpWage("BRIDGELAB", 15, 18, 100);
             Console.ReadLine();
         }
     }
